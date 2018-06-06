3ab39c8e686ef4d37beec5ccfca7f1e697509fa4
carvinw101 <35495619+carvinw101@users.noreply.github.com>
2018-06-06 12:12:49 +0700

    Add files via upload

---
+++ root/application/models/New_model.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_name_model extends CI_Model {
+
+	public function __construct() 
+	{
+		parent::__construct();
+	}
+
+	public function Foo()
+	{
+		//some code...
+	}
+
+}
